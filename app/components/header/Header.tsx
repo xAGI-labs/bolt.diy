98414af6433b08f2c4e6e2e03187b733eedcc2cd
niraj-jha <jhaniraj45@gmail.com>
2024-12-31 16:14:10 +0530

    Updated the Logo of the Website

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -108,11 +108,7 @@ export function Header() {
       <div className="w-full flex items-center justify-between gap-2 z-logo text-bolt-elements-textPrimary cursor-pointer">
         <div className="flex gap-1 items-center justify-center">
           <div className="i-ph:sidebar-simple-duotone text-xl" />
-          <a href="/" className="text-2xl font-semibold text-accent flex items-center">
-            {/* <span className="i-bolt:logo-text?mask w-[46px] inline-block" /> */}
-            <img src="/logo-light-styled.png" alt="logo" className="w-[90px] inline-block dark:hidden" />
-            <img src="/logo-dark-styled.png" alt="logo" className="w-[90px] inline-block hidden dark:block" />
-          </a>
+          <img src="/mainLogo.png" alt="Main Logo" className="w-[45px]" />
         </div>
         <div className="flex gap-2 items-center justify-center">
           <SignedIn>
