c073dbe23a83f64f79823787afb1e78467c3965f
niraj-jha <jhaniraj45@gmail.com>
2025-02-22 13:41:13 +0530

    Runnig fix checking - 1

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -506,7 +506,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
                         minHeight: TEXTAREA_MIN_HEIGHT,
                         maxHeight: TEXTAREA_MAX_HEIGHT,
                       }}
-                      placeholder="How can App Desk help you today?"
+                      placeholder="How can AppDesk help you today?"
                       translate="no"
                     />
                     <ClientOnly>
